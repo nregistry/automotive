ebd1b20afd69455b698c634e6b84acc3d194c7a9
steve kama <stevekamahertz@gmail.com>
2020-12-24 18:11:22 +0300

    admins table migration

---
+++ super_admin/index.php
@@ -0,0 +1,6 @@
+<?php require_once('../init/initialization.php');
+$title = "Schedulize || Super Admin";
+require_once(PUBLIC_PATH  . DS . "layouts" . DS . "super_admin" . DS . "header.php"); ?>
+
+
+<?php require_once(PUBLIC_PATH . DS . "layouts" . DS . "super_admin" . DS . "footer.php"); ?>
