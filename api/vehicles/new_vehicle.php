64389dfc5f6cc4141b631010697a654acd11b6cb
steve kama <stevekamahertz@gmail.com>
2020-12-28 16:15:35 +0300

    new vehicle request

---
+++ api/vehicles/new_vehicle.php
@@ -0,0 +1,32 @@
+<?php 
+
+// Headers
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+header('Access-Control-Allow-Methods: POST');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
+
+require_once('../../init/initialization.php');
+
+$d = new DateTime();
+
+$data = array();
+
+$vehicle = new Vehicles();
+
+$vehicle->member_id = $_POST['member_id'];
+$vehicle->vin_number = $_POST['vin_number'];
+$vehicle->profile = 'noimage.png';
+$vehicle->production_date = $_POST['production_date'];
+$p_date = new DateTime($vehicle->production_date);
+$vehicle->year = $p_date->format('Y');
+$vehicle->model = $_POST['model'];
+$vehicle->engine = $_POST['engine'];
+$vehicle->trans = $_POST['trans'];
+$vehicle->status = 'REQUEST';
+$vehicle->timestamp = $d->format('Y-m-d H:i:s');
+
+if($vehicle->save()){
+    $data['message'] = 'success';
+}
+echo json_encode($data);
--- members/vehicles/requests.php
+++ members/vehicles/requests.php
@@ -31,7 +31,7 @@ require_once(PUBLIC_PATH  . DS . "layouts" . DS . "users" . DS . "header.php"); 
                 <div class="card-header">
                     <h3 class="card-title">Requested Vehicles Table</h3>
                     <div class="card-tools">
-                        <a href="#" class="btn btn-tool btn-sm btn-success">
+                        <a href="#" id="newVehicleRequestBtn" class="btn btn-tool btn-sm btn-success">
                             <i class="fa fa-plus"></i> MAKE REQUEST
                         </a>
                     </div>
@@ -62,6 +62,57 @@ require_once(PUBLIC_PATH  . DS . "layouts" . DS . "users" . DS . "header.php"); 
         <!-- /.col -->
     </div>
     <!-- /.row -->
+    <div class="modal fade" id="newVehicleRequestModal">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <form id="newVehicleRequestForm" autocomplete="off">
+                    <div class="modal-header">
+                        <h4 class="modal-title">New Vehicle Request</h4>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="form-group">
+                            <input type="hidden" id="newVehicleRequestsMemberId" class="form-control" name="member_id" />
+                        </div>
+
+                        <div class="form-group">
+                            <label for="newVehicleRequestsVinNumber">VIN Nunber</label>
+                            <input type="text" id="newVehicleRequestsVinNumber" class="form-control" name="vin_number" placeholder="Enter VIN Number" />
+                        </div>
+
+                        <div class="form-group">
+                            <label for="newVehicleRequestsProductionDate">Production Date</label>
+                            <input type="text" id="newVehicleRequestsProductionDate" class="form-control datepicker" name="production_date" placeholder="Enter Vehicle Production Date" />
+                        </div>
+
+                        <div class="form-group">
+                            <label for="newVehicleRequestsModel">Vehicle Model</label>
+                            <input type="text" id="newVehicleRequestsModel" class="form-control" name="model" placeholder="Enter Vehicle Model" />
+                        </div>
+
+                        <div class="form-group">
+                            <label for="newVehicleRequestsEngine">Engine</label>
+                            <input type="text" id="newVehicleRequestsEngine" class="form-control" name="engine" placeholder="Enter Vehicle Engine" />
+                        </div>
+
+                        <div class="form-group">
+                            <label for="newVehicleRequestsTrans">Trans</label>
+                            <input type="text" id="newVehicleRequestsTrans" class="form-control" name="trans" placeholder="Enter Vehicle Trans" />
+                        </div>
+                    </div>
+                    <div class="modal-footer justify-content-between">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                        <button type="submit" id="newVehicleRequestsSubmitBtn" class="btn btn-primary">Save</button>
+                    </div>
+                </form>
+            </div>
+            <!-- /.modal-content -->
+        </div>
+        <!-- /.modal-dialog -->
+    </div>
+    <!-- /.modal -->
 </section>
 <!-- /.content -->
 
@@ -88,29 +139,49 @@ require_once(PUBLIC_PATH  . DS . "layouts" . DS . "users" . DS . "header.php"); 
             });
         }
 
-        $(document).on('click', '.view', function() {
-            var customer_id = $(this).attr('id');
-            var action = "FETCH_CUSTOMER";
+        $('#newVehicleRequestBtn').click(function(e) {
+            e.preventDefault();
+            var action = "FETCH_LOGGED_IN_USER";
             $.ajax({
-                url: "<?php echo base_url(); ?>api/customers/customers.php",
+                url: "<?php echo base_url(); ?>api/members/members.php",
                 type: "POST",
                 data: {
                     action: action,
-                    customer_id: customer_id
                 },
                 dataType: "json",
                 success: function(data) {
-                    if (data.message == 'errorCustomer') {
-                        toastr.error('The customer selected does not exist');
-                        // .return false;
-                    } else {
-                        var cust_id = $.trim(data.id);
-                        localStorage.setItem('customer_id', cust_id);
-                        window.location.href = '<?php echo base_url(); ?>admin/customers/view.php?organization=' + organization_id + '&customer=' + cust_id;
+                    $('#newVehicleRequestsMemberId').val(data.id);
+                    $('#newVehicleRequestModal').modal('show');
+                }
+            });
+        });
+
+        $('#newVehicleRequestForm').submit(function(event){
+            event.preventDefault();
+            var form_data = $(this).serialize();
+            $.ajax({
+                url: "<?php echo base_url(); ?>api/vehicles/new_vehicle.php",
+                type: "POST",
+                data: form_data,
+                dataType: "json",
+                beforeSend:function(){
+                    $('#newVehicleRequestsSubmitBtn').html('Loading...');
+                },
+                success: function(data) {
+                    if(data.message == 'success'){
+                        $('#newVehicleRequestsSubmitBtn').html('Success');
+                        $('#loadVehicles').DataTable().destroy();
+                        find_vehicles(); 
+                        $('#newVehicleRequestForm')[0].reset();
+                        $('#newVehicleRequestModal').modal('hide');
                     }
                 }
             });
+        });
 
+        $(document).on('click', '.view', function(){
+            var vehicle_id = $(this).attr('id');
+            alert(vehicle_id);
         });
     });
 </script>
